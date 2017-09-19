7d4c3919023564eb5fda3602361940642a465f43
Luisangonzalez <luisangonzalez@gmail.com>
2017-09-19 21:44:14 +0200

    allowed all cors

--- back/server/routes/index.js
+++ back/server/routes/index.js
@@ -1,7 +1,12 @@
 import { api } from './api';
 
+import cors from 'cors';
+
+// TODO Enable all cors, change with enviroment variable
+
 export let routes = (app) => {
 
+  app.use(cors(true));
   api(app);
 
 };
