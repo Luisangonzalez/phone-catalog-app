565c4853e1f9e94992a332386213ae0e0c41de9c
Luisangonzalez <luisangonzalez@gmail.com>
2017-09-19 21:16:31 +0200

    remove cors and add allowed all origin

--- back/server/app.js
+++ back/server/app.js
@@ -9,6 +9,14 @@ app.use(bodyParser.json());
 
 routes(app);
 
+// Allowed all origin (only in dev)
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
+
 // Static folder
 app.use('/', express.static('../front/dist'));
 
--- back/server/routes/index.js
+++ back/server/routes/index.js
@@ -1,23 +1,7 @@
 import { api } from './api';
 
-import cors from 'cors';
-
-
-// let whitelist = [process.env.FRONTEND_URI];
-let whitelist = ['http://localhost:8080', 'http://localhost:5000'];
-let corsOptions = {
-  origin: function (origin, callback) {
-    if (whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS !'));
-    }
-  }
-};
-
 export let routes = (app) => {
 
-  app.use(cors(corsOptions));
   api(app);
 
 };
